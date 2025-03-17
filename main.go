b3b2d556cf0a63218593b38f60c02f7386072165
mjh1207 <mitchh1207@gmail.com>
2025-03-17 12:21:36 -0600

    Health check endpoint

--- main.go
+++ main.go
@@ -8,8 +8,9 @@ import (
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
 	server := &http.Server {
 		Handler: mux,
 		Addr: ":" + port,
@@ -18,4 +19,10 @@ func main() {
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
