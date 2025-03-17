ee637d7c576ce5bbdbc11d98cca79bf1aa9aa19a
mjh1207 <mitchh1207@gmail.com>
2025-03-17 11:51:33 -0600

    Add logo image

--- main.go
+++ main.go
@@ -8,10 +8,13 @@ import (
 func main() {
 	const port = "8080"
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	
 	server := &http.Server {
 		Handler: mux,
 		Addr: ":" + port,
 	}
+
 
 	log.Printf("Serving on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
