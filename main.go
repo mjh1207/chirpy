016f3fd2d26445e1fa502f97ced44e6b2fd17f4a
mjh1207 <mitchh1207@gmail.com>
2025-03-19 12:47:41 -0600

    db connection

--- main.go
+++ main.go
@@ -1,20 +1,40 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/mjh1207/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	dbConn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("unable to open sql connection: %v", err)
+	}
+
 	const port = "8080"
 	apiCfg := apiConfig {
 		fileserverHits: atomic.Int32{},
+		db: database.New(dbConn),
 	}
+
 
 	mux := http.NewServeMux()
 	handler := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
